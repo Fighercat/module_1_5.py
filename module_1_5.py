a01fd96c0b4cc424de933afb83031047d21fc79d
Fighercat <artem.sitnikov.2002@list.ru>
2024-08-27 00:20:41 +0500

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -3,9 +3,16 @@ immutable_var = (1,'b','_',True)
 #кортеж - используется в качестве хранилища, например, для какого-то списка, который мы ни коим образом не хотим трогать, то есть нам нужно чтобы он оставался неизменным.
 print('Immutable tuple:',immutable_var)
 print(type(immutable_var))
-mutable_list = ([(1,'b','_',True)],2)*(2)
+mutable_list = [str]
+mutable_list =(1,2,3,4)
 print('mutable_list:',mutable_list)
+print(type(mutable_list))
+mutable_list=list(mutable_list)
+print(type(mutable_list))
+mutable_list.remove(1)
+mutable_list[1]=300
 #mutable_list =
 #mutable_list.remove (0)
-mutable_list[0][0] =3
 print('mutable_list:',mutable_list)
+
+
